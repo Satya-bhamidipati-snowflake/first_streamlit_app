b78759200024070c734401c68386c377e60a44bd
Satya-bhamidipati-snowflake <153896858+Satya-bhamidipati-snowflake@users.noreply.github.com>
2023-12-18 22:32:03 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,8 @@
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+import streamlit
+streamlit.title ('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast ')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
