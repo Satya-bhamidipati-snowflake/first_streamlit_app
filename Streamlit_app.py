49d3a4ec2100928e737ef017dcd2cd0b44f67699
Satya-bhamidipati-snowflake <153896858+Satya-bhamidipati-snowflake@users.noreply.github.com>
2023-12-19 13:45:15 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,17 +1,1 @@
-import streamlit
-streamlit.title ('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast ')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
---fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
---streamlit.write('The user entered ', fruit_choice)
---import requests
---fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice")
-
-
 
